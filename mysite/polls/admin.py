02bdb5fef30ae60c74cc63add7fdfbfbb4208dda
ashu9999 <kmr.ashutosh9999@gmail.com>
2020-04-07 17:23:35 +0530

    database created

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question, Choices
+
+admin.site.site_header = "Polls Admin"
+admin.site.site_title = "Polls Admin Area"
+admin.site.index_title = "Welcome to Polls admin area"
+
+
+# admin.site.register(Question)
+# admin.site.register(Choices)
+
+class ChoicesInline(admin.TabularInline):
+    model = Choices
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+    ('Date Information', {'fields': ['pub_date'], 'classes': 
+    ['collapse']}),]
+    inlines = [ChoicesInline]
+
+
+admin.site.register(Question, QuestionAdmin)    
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-    question_text = models.CharField(max_length==200)
+    question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
@@ -13,5 +13,5 @@ class Choices(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    def __str__(self)
+    def __str__(self):
         return self.choice_text
