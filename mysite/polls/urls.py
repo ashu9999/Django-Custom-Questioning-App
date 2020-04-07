328dd0e950195fa8e3556ff057269c4655dd2d80
ashu9999 <kmr.ashutosh9999@gmail.com>
2020-04-07 23:54:30 +0530

    starting views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:question_id>', views.detail, name='detail')
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,4 +4,14 @@ from .models import Question, Choices
 
 # get question and display
 def index(request):
-    return render(request, 'polls/index.html')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list':latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+#specific question and  choices
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("qustion does'nt exist")
+    return render(request, 'polls/results.html', {'question': question})
