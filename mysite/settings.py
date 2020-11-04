e82f661379a81f8ab39b7d2c6c2819fa0b14675b
ashu9999 <kmr.ashutosh9999@gmail.com>
2020-11-05 01:24:06 +0530

    cp

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'questionapp',
+        'USER': 'admin',
+        'PASSWORD': 'fghjkl@123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
